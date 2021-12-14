631063ec4388fd513621ae12bff690c5a51243bd
khadetou <khadetou96@gmail.com>
2021-12-14 17:31:40 +0000

    all goals items

--- src/pages/AllGoals.tsx
+++ src/pages/AllGoals.tsx
@@ -1,7 +1,22 @@
-import { IonButtons, IonContent, IonHeader, IonMenuButton, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 import React from 'react'
+import { IonButtons, IonContent, IonHeader, IonItem, IonLabel, IonList, IonMenuButton, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { COURSE_DATA } from './Courses';
 
 const AllGoals: React.FC = () => {
+    const goals = COURSE_DATA.map((course) => {
+        return course.goals.map((goal) => {
+            return { ...goal, courseName: course.name }
+        });
+    }).reduce((goalArr, nestedGoals) => {
+        let updatedGoalArray = goalArr;
+        for (const goal of nestedGoals) {
+            updatedGoalArray = updatedGoalArray.concat(goal);
+        }
+        return updatedGoalArray;
+    }, []);
+
+    console.log(goals);
+
     return (
         <IonPage>
             <IonHeader>
@@ -15,7 +30,17 @@ const AllGoals: React.FC = () => {
                 </IonToolbar>
             </IonHeader>
             <IonContent>
-                <h2>This works - all goals page!</h2>
+                <IonList>
+                    {goals.map((goal) => (
+                        <IonItem key={goal.id} >
+                            <IonLabel>
+                                <h2>{goal.text}</h2>
+                                <p>{goal.courseName}</p>
+                            </IonLabel>
+                        </IonItem>
+                    ))
+                    }
+                </IonList>
             </IonContent>
         </IonPage>
     )
