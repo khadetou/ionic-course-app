4ed64f5628c26a4dbb1c90967105a4c4b5b15978
khadetou <khadetou96@gmail.com>
2021-12-14 17:38:54 +0000

    filter IonToggle

--- src/pages/Filter.tsx
+++ src/pages/Filter.tsx
@@ -1,7 +1,12 @@
-import { IonButtons, IonContent, IonHeader, IonMenuButton, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 import React from 'react'
+import { IonButtons, IonContent, IonHeader, IonItem, IonLabel, IonList, IonMenuButton, IonPage, IonTitle, IonToggle, IonToolbar } from '@ionic/react';
+import { COURSE_DATA } from './Courses';
+
 
 const Filter: React.FC = () => {
+    const courseFilterChangeHandler = (event: CustomEvent) => {
+        console.log(event);
+    }
     return (
         <IonPage>
             <IonHeader>
@@ -13,7 +18,14 @@ const Filter: React.FC = () => {
                 </IonToolbar>
             </IonHeader>
             <IonContent>
-                <h2>The filter page...</h2>
+                <IonList>
+                    {COURSE_DATA.map((course) => (
+                        <IonItem key={course.id}>
+                            <IonLabel>{course.name}</IonLabel>
+                            <IonToggle value={course.id} onIonChange={courseFilterChangeHandler} />
+                        </IonItem>
+                    ))}
+                </IonList>
             </IonContent>
         </IonPage>
     )
