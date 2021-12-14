db414ee974cab735a9ed647942ea08958ec4a493
khadetou <khadetou96@gmail.com>
2021-12-14 11:26:47 +0000

    deleteItemHandler && startEditGlobalHandler

--- src/pages/CourseGoal.tsx
+++ src/pages/CourseGoal.tsx
@@ -1,11 +1,20 @@
 import React from 'react'
-import { IonBackButton, IonButtons, IonContent, IonHeader, IonItem, IonLabel, IonList, IonPage, IonTitle, IonToolbar } from '@ionic/react'
+import { IonBackButton, IonButton, IonButtons, IonContent, IonHeader, IonIcon, IonItem, IonLabel, IonList, IonPage, IonTitle, IonToolbar } from '@ionic/react'
 import { COURSE_DATA } from './Courses';
 import { useParams } from 'react-router';
+import { create } from 'ionicons/icons';
 
 const CourseGoal: React.FC = () => {
     const seletedCourseId = useParams<{ courseId: string }>().courseId;
     const selectedCourse = COURSE_DATA.find(course => course.id === seletedCourseId);
+    const deleteItemHandler = () => {
+        console.log("Delete item...");
+    }
+    const startEditGlobalHandler = (event: React.MouseEvent) => {
+        event.stopPropagation();
+        console.log("Start edit global...");
+
+    }
     return (
         <IonPage>
             <IonHeader>
@@ -20,8 +29,11 @@ const CourseGoal: React.FC = () => {
                 {selectedCourse && (
                     <IonList>
                         {selectedCourse.goals.map(goal => (
-                            <IonItem key={goal.id}>
+                            <IonItem key={goal.id} lines="full" button onClick={deleteItemHandler}>
                                 <IonLabel>{goal.text}</IonLabel>
+                                <IonButton slot="end" onClick={startEditGlobalHandler}>
+                                    <IonIcon slot="icon-only" icon={create} />
+                                </IonButton>
                             </IonItem>
                         ))}
                     </IonList>
