00f8767253fb6aaaf0cb505c24764ecaa7c38a19
khadetou <khadetou96@gmail.com>
2021-12-14 17:08:52 +0000

    ionDatetitme

--- src/components/AddCourseModal.tsx
+++ src/components/AddCourseModal.tsx
@@ -1,5 +1,5 @@
-import React from 'react'
-import { IonButton, IonCol, IonContent, IonGrid, IonHeader, IonInput, IonItem, IonLabel, IonModal, IonRow, IonTitle, IonToolbar } from '@ionic/react';
+import React, { useRef } from 'react'
+import { IonButton, IonButtons, IonCol, IonContent, IonDatetime, IonGrid, IonHeader, IonInput, IonItem, IonLabel, IonModal, IonRow, IonTitle, IonToolbar } from '@ionic/react';
 
 
 interface AddCourseModalProps {
@@ -7,6 +7,13 @@ interface AddCourseModalProps {
     onDismiss: () => void;
 }
 const AddCourseModal: React.FC<AddCourseModalProps> = ({ show, onDismiss }) => {
+    const customDateTime = useRef<HTMLIonDatetimeElement>(null);
+    const reset = () => {
+        customDateTime.current!.value = '';
+    }
+    const confirm = () => {
+        console.log("confirm!")
+    }
     return (
         <IonModal isOpen={show}>
             <IonHeader>
@@ -24,12 +31,27 @@ const AddCourseModal: React.FC<AddCourseModalProps> = ({ show, onDismiss }) => {
                             </IonItem>
                         </IonCol>
                     </IonRow>
+                    <IonRow>
+                        <IonCol>
+                            <IonItem>
+                                <IonLabel>Enrolment Date</IonLabel>
+                                <IonDatetime ref={customDateTime} >
+                                    <IonButtons>
+                                        <IonButton onClick={() => confirm()}>
+                                            Good To Go!
+                                        </IonButton>
+                                        <IonButton onClick={() => reset()}>Reset</IonButton>
+                                    </IonButtons>
+                                </IonDatetime>
+                            </IonItem>
+                        </IonCol>
+                    </IonRow>
                     <IonRow className="ion-text-center">
                         <IonCol>
                             <IonButton color="dark" fill="clear" onClick={onDismiss}>Cancel</IonButton>
                         </IonCol>
                         <IonCol>
-                            <IonButton color="secondary">Save</IonButton>
+                            <IonButton expand="block" color="secondary">Save</IonButton>
                         </IonCol>
                     </IonRow>
                 </IonGrid>
